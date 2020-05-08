8ae16db1c6dc45a190afce5b353b5609d26ea271
Adam Fishman <afishman@cs.washington.edu>
2020-05-07 21:08:31 -0700

    Initial Commit

---
+++ Mobile Systems Project 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile Systems Project 2
+//
+//  Created by Adam Fishman on 5/7/20.
+//  Copyright © 2020 Adam Fishman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
